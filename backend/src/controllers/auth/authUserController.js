29f9f669c250377f01a05f2d4ca1c229bc945b6a
Nitesh Lakhera <nitslakhera@gmail.com>
2026-04-27 17:41:00 +0530

    created Register,Verify-otp and login API.

--- backend/src/controllers/auth/authUserController.js
+++ backend/src/controllers/auth/authUserController.js
@@ -41,3 +41,67 @@ export const registerUser = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+export const verifyOtp = async (req, res) => {
+    try {
+        const {email , otp} = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        if(user.otp !== otp || user.otpExpiry < Date.now()) {
+            return res.status(400).json({ message: "Invalid or expired OTP" });
+        }
+
+        user.isVerified = true;
+        user.otp = null;
+        user.otpExpiry = null;
+        await user.save();
+        res.status(200).json({ message: "OTP verified successfully" });
+        
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const LoginUser = async (req, res) => {
+    try {
+            const { email ,password , role} = req.body;
+
+            //check user exists or not
+            const userExists = await User.findOne({ email });
+            if(!userExists){
+                return res.status(404).json({ message: "User not found" });
+            }
+
+            // Check if the user is verified
+            if(!userExists.isVerified){
+                return res.status(400).json({ message: "Please verify your email first" });
+            }
+
+            //check password
+            const isPasswordValid = await bcrypt.compare(password, userExists.password);
+            if(!isPasswordValid){
+                return res.status(400).json({ message: "Invalid password" });
+            }
+
+            //check role
+            if(userExists.role !== role){
+                return res.status(400).json({ message: "Invalid role" });
+            }
+
+            res.status(200).json({ message: "Login successful" , user: {
+                id: userExists._id,
+                firstName: userExists.firstName,
+                lastName: userExists.lastName,
+                email: userExists.email,
+                role: userExists.role
+            } });
+
+        
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
--- backend/src/routes/auth/authRoute.js
+++ backend/src/routes/auth/authRoute.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { registerUser } from '../../controllers/auth/authUserController.js';
+import { LoginUser, registerUser, verifyOtp } from '../../controllers/auth/authUserController.js';
 
 const router = express.Router();
 
 router.post('/register' , registerUser)
+router.post('/verify-otp' , verifyOtp);
+router.post('/login' , LoginUser);
 
 export default router;
